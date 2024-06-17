7a8a256b84f9dd0ac1bf98026320602ccb48419d
gayathri <gayathriande20@gmail.com>
2024-06-17 22:16:40 +0530

    first commit

---
+++ src/Header/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Mainimg from "../Components/Mainimg";
+import Footer from "../Components/footer";
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Mainimg />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Header/Project.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/footer";
+const Project = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
+
+export default Project;
